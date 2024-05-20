a5887049e05ad667727bfea88838f94bd6b16fdb
akradadiya28 <akradadiya28@gmail.com>
2024-05-20 18:55:49 +0530

    Task-10

---
+++ Task-10/js/script.js
@@ -0,0 +1,18 @@
+// Write a JAVASCRIPT Program to swap a value without third variable using with argument no return type.
+
+const swap = (a, b) => {
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    document.getElementById("after1").innerHTML = (`After Swapping Number A is = ${a}`);
+    document.getElementById("after2").innerHTML = (`After Swapping Number B is = ${b}`);
+}
+
+let a = 10, b = 20;
+
+document.getElementById("before1").innerHTML = (`Before Swapping Number A is = ${a}`);
+document.getElementById("before2").innerHTML = (`Before Swapping Number B is = ${b}`);
+
+swap(a, b);
