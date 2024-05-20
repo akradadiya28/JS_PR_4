fa38a5d98c5d8a76ea9d8895086fd7fc8467dd46
akradadiya28 <akradadiya28@gmail.com>
2024-05-20 15:15:27 +0530

    Task-3

---
+++ Task-3/js/script.js
@@ -0,0 +1,18 @@
+// Write a JAVASCRIPT Program to find Triangle area ( area = (l*h)/2 ) using return type with argument function.
+
+const areaOfTriangle = (l, h) => {
+
+    area = (l * h) / 2;
+    return (area);
+}
+
+let l, h, area;
+
+l = 10;
+h = 20;
+
+area = areaOfTriangle(l, h);
+
+document.getElementById("length").innerHTML = (`Length is = ${l}`);
+document.getElementById("height").innerHTML = (`Heght is = ${h}`);
+document.getElementById("area").innerHTML = (`Triangle of Area is = ${area}`);
